94f5f65dc83640fca04643e61d87c5d0318777be
Maycon Douglas Niculau dos Santos <60269933+Maycondo@users.noreply.github.com>
2023-12-19 23:37:38 +0000

    Portifolio

--- script.js
+++ script.js
@@ -1,40 +1,25 @@
-const player = document.querySelector("#player");
-const display = document.querySelector("#display");
-const authorName = document.querySelector("#authorName");
-const musicName = document.querySelector("#musicName");
-const playerPausebtn = document.querySelector("#playerPausebtn");
-const prevButton = document.querySelector("#prevButton");
-const nextButton = document.querySelector("#nextButton");
-const currentTime = document.querySelector("#currentTime");
-const progressBar = document.querySelector("#progress-bar");
-const progress = document.querySelector(".progress");
 
-import songs from "songs.js";
 
-const textButtonPlay = `<ion-icon name="play"></ion-icon>`;
-const textButtonPause = `<ion-icon name="pause"></ion-icon>`;
 
-let index = 0;
+const TituloPrincipal = document.getElementById("Name-User");
 
-prevButton.onclick = () => prevNextMusic("prev");
-nextButton.onclick = () => prevNextMusic();
+const Messegens = [
+  "Welcome to my portfolio",
+  "My name is Michael",
+  "I'm a full stack developer",
+];
 
-playerPausebtn.onclick = () => playerPausebtn();
+let indexAtual = 0;
 
-const prevNextMusic = (type = "next") => {
-  if ((type == "next" && index + 1 === songs.length) || type === "init") {
-    index = 0;
-  } else if (type == "prev" && index === 0) {
-    index = songs.length;
-  } else {
-    index = type === "prev" && index ? index - 1 : index + 1;
-  }
+function Animetion() {
+  TituloPrincipal.innerHTML = Messegens[indexAtual];
 
-  player.src = songs[index].src;
-  musicName.innerHTML = songs[index].name;
-  authorName.innerHTML = songs[index].author;
-  display.display = songs[index].display;
-  if (type !== "init") playerPausebtn();
-};
+  indexAtual = (indexAtual + 1) % Messegens.length;
+}
 
-prevNextMusic("init");
+let intervaloTroca = setInterval(Animetion, 2000);
+
+setTimeout(function () {
+  clearInterval(intervaloTroca);
+  TituloPrincipal.innerHTML = "Welcome to my portfolio";
+}, 10000);
