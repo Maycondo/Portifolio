f12faa5706186fd2babb727935cbe0beb2a38664
Maycon Douglas <maycondouglasniculau@gmail.com>
2024-09-19 16:45:38 -0300

    Aplicando a responsividade do projeto

--- src/components/SliderInfinito/index.jsx
+++ src/components/SliderInfinito/index.jsx
@@ -1,52 +1,51 @@
 import {
-    SiJavascript,
-    SiHtml5,
-    SiCss3,
-    SiPython,
-    SiReact,
-    SiDjango,
-    SiNodedotjs,
-    SiGit,
-    SiGithub,
-    SiPostgresql,
-    SiCsharp 
-  } from "react-icons/si";
+  SiJavascript,
+  SiHtml5,
+  SiCss3,
+  SiPython,
+  SiReact,
+  SiDjango,
+  SiNodedotjs,
+  SiGit,
+  SiGithub,
+  SiPostgresql,
+  SiCsharp
+} from "react-icons/si";
+import "./style.css"
 
-  
-  const LOGOS = [
-    // eslint-disable-next-line react/jsx-key
-    <SiJavascript size={45} style={{ color: "gray" }} />,
-    // eslint-disable-next-line react/jsx-key
-    <SiHtml5 size={45} style={{ color: "gray" }} />,
-    // eslint-disable-next-line react/jsx-key
-    <SiCss3 size={45} style={{ color: "gray" }} />,
-    // eslint-disable-next-line react/jsx-key
-    <SiPython size={45} style={{ color: "gray" }} />,
-    // eslint-disable-next-line react/jsx-key
-    <SiReact size={45} style={{ color: "gray" }} />,
-    // eslint-disable-next-line react/jsx-key
-    <SiDjango size={45} style={{ color: "gray" }} />,
-    // eslint-disable-next-line react/jsx-key
-    <SiNodedotjs size={45} style={{ color: "gray" }} />,
-    // eslint-disable-next-line react/jsx-key
-    <SiGit size={45} style={{ color: "gray" }} />,
-    // eslint-disable-next-line react/jsx-key
-    <SiGithub size={45} style={{ color: "gray" }} />,
-    // eslint-disable-next-line react/jsx-key
-    <SiPostgresql size={45} style={{ color: "gray" }} />, // Representando SQL
-    // eslint-disable-next-line react/jsx-key
-    <SiCsharp size={45} style={{ color: "gray" }} />
-  ];
-  
-  
-  export default function InfiniteSlider() {
-    return (
-     <>
-      <div className="relative m-4 h-40 w-full overflow-hidden before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100  after:content-['']">
+const LOGOS = [
+     // eslint-disable-next-line react/jsx-key
+  <SiJavascript className="icon icon-mobile icon-tablet icon-desktop" />,
+     // eslint-disable-next-line react/jsx-key
+  <SiHtml5 className="icon icon-mobile icon-tablet icon-desktop" />,
+       // eslint-disable-next-line react/jsx-key
+  <SiCss3 className="icon icon-mobile icon-tablet icon-desktop" />,
+       // eslint-disable-next-line react/jsx-key
+  <SiPython className="icon icon-mobile icon-tablet icon-desktop" />,
+       // eslint-disable-next-line react/jsx-key
+  <SiReact className="icon icon-mobile icon-tablet icon-desktop" />,
+       // eslint-disable-next-line react/jsx-key
+  <SiDjango className="icon icon-mobile icon-tablet icon-desktop" />,
+       // eslint-disable-next-line react/jsx-key
+  <SiNodedotjs className="icon icon-mobile icon-tablet icon-desktop" />,
+       // eslint-disable-next-line react/jsx-key
+  <SiGit className="icon icon-mobile icon-tablet icon-desktop" />,
+       // eslint-disable-next-line react/jsx-key
+  <SiGithub className="icon icon-mobile icon-tablet icon-desktop" />,
+       // eslint-disable-next-line react/jsx-key
+  <SiPostgresql className="icon icon-mobile icon-tablet icon-desktop" />,
+       // eslint-disable-next-line react/jsx-key
+  <SiCsharp className="icon icon-mobile icon-tablet icon-desktop" />
+];
+
+export default function InfiniteSlider() {
+  return (
+    <>
+      <div className="relative m-4 h-40 w-full overflow-hidden before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:content-['']">
         <div className="animate-infinite-slider flex w-[calc(250px*22)] mt-4">
           {LOGOS.map((logo, index) => (
             <div
-              className="slide flex w-[8vw] items-center justify-center"
+              className="slide flex w-[12vw] sm:w-[8vw] md:w-[6vw] lg:w-[5vw] items-center justify-center"
               key={index}
             >
               {logo}
@@ -54,7 +53,7 @@ import {
           ))}
           {LOGOS.map((logo, index) => (
             <div
-              className="slide flex w-[8vw] items-center justify-center"
+              className="slide flex w-[12vw] sm:w-[8vw] md:w-[6vw] lg:w-[6vw] items-center justify-center"
               key={index}
             >
               {logo}
@@ -64,7 +63,7 @@ import {
         <div className="animate-infinite-slider-reverse flex w-[calc(250px*22)] mt-4">
           {LOGOS.map((logo, index) => (
             <div
-              className="slide flex w-[8vw] items-center justify-center"
+              className="slide flex w-[12vw] sm:w-[8vw] md:w-[6vw] lg:w-[6vw] items-center justify-center"
               key={index}
             >
               {logo}
@@ -72,7 +71,7 @@ import {
           ))}
           {LOGOS.map((logo, index) => (
             <div
-              className="slide flex w-[8vw] items-center justify-center"
+              className="slide flex w-[12vw] sm:w-[8vw] md:w-[6vw] lg:w-[6vw] items-center justify-center"
               key={index}
             >
               {logo}
@@ -81,6 +80,5 @@ import {
         </div>
       </div>
     </>
-    );
-  }
-  
+  );
+}
