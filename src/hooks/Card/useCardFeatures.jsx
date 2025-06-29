7adbb528f5adee24aa6fa0cd963fbfb31496eb93
Maycon Douglas <maycondouglasniculau@gmail.com>
2025-06-29 19:21:59 -0300

    Ajustando estilos no componente Card para melhorar a apresentação e responsividade. Atualizando propriedades de alinhamento e dimensões. Adicionando informações sobre tecnologias utilizadas nos projetos.

--- src/hooks/Card/useCardFeatures.jsx
+++ src/hooks/Card/useCardFeatures.jsx
@@ -13,21 +13,26 @@ export default function CardFeatures({  handleLanguageChange }) {
             description: 'Criação de um bot interativo para gerenciar servidores do Discord. Implementação da biblioteca discord.py para permitir a interação com API Discord. O bot oferece comandos personalizados para moderar e organizar servidores do Discord Responde os usuários com comandos pre-definidos, tornando a interações com usuários mais fluida...',
             image: 'https://tecdn.b-cdn.net/img/new/standard/city/044.webp',
             link: 'https://github.com/Maycondo/DiscordBot',
+            tech: 'Python, Discord.py',
 
         },
+
         {
             title: "MyBooks Website",
             description: 'My-Books é um site onde vou cadastrar todos os livros que estou lendo. Em cada cadastro, haverá uma breve descrição do livro e algumas opiniões pessoais sobre aspectos abordados na leitura. Também haverá uma seção de favoritos, onde serão exibidos todos os livros que marquei como favoritos.',
             image: 'https://tecdn.b-cdn.net/img/new/standard/city/043.webp',
             link: 'https://github.com/Maycondo/my-books',
             site: 'https://my-books-wheat.vercel.app/',
+            tech: 'React, TypeScript, Node.js, Express, MongoDB',
         },
+
         {
             title: "Web iBooks",
             description: 'O iBooks é um projeto pessoal desenvolvido para consolidar e demonstrar minhas habilidades em desenvolvimento web. Ele faz requisições à API do Google Books para obter informações detalhadas sobre livros, incluindo autores, descrições, avaliações e capas. Os usuários podem criar uma lista personalizada de favoritos para acessar seus livros preferidos.',
             image: 'https://tecdn.b-cdn.net/img/new/standard/city/042.webp',
             link: 'https://github.com/Maycondo/WebIbooks',
             site: 'https://web-ibooks.vercel.app/',
+            tech: 'React, Node.js, Express, MongoDB',
         },
         // ...demais projetos omitidos por brevidade
     ];
